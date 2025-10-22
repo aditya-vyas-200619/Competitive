f38946e6adf750460f47c87059695f5f6e0fda4a
Aditya Vyas <aditya.20253025@mnnit.ac.in>
2025-10-22 09:09:19 +0530

    22nd October, 2025

--- Arrays/2D/multiplication.c
+++ Arrays/2D/multiplication.c
@@ -1,50 +1,63 @@
 #include <stdio.h>
 int main()
 {
-    int r1,r2,c1,c2;
-    printf("------FOR MATRIX_1------\n");
-    printf("Enter no. of rows: \n");
-    scanf("%d",&r1);
-    printf("Enter no. of columns: \n");
-    scanf("%d",&c1);
-    printf("Enter elements of Matrix 1: \n");
-    int m1[r1][c1];
-    int m2[r2][c2];
+  int r1,r2,c1,c2;
+  printf("------FOR MATRIX 1------\n");
+  printf("Enter r1: ");
+  scanf("%d",&r1);
+  printf("Enter c1: ");
+  scanf("%d",&c1);
+  int arr1[r1][c1];
+  printf("Enter the elements: \n");
+  for(int i=0;i<r1;i++)
+  {
+    for(int j=0;j<c1;j++)
+    {
+      scanf("%d",&arr1[i][j]);
+    }
+  }
+  printf("------FOR MATRIX 2------\n");
+  printf("Enter r2: ");
+  scanf("%d",&r2);
+  printf("Enter c2: ");
+  scanf("%d",&c2);
+  printf("Enter the elements: \n");
+  int arr2[r2][c2];
+  for(int i=0;i<r2;i++)
+  {
+    for(int j=0;j<c2;j++)
+    {
+      scanf("%d",&arr2[i][j]);
+    }
+  }
+  if(c1!=r2)
+  {
+    printf("Matrix multiplication not possible.");
+  }
+  else
+  {
+    int cr = r2;
+    int res[r1][c2];
     for(int i=0;i<r1;i++)
     {
-        for(int j=0;j<c1;j++)
-        {
-            scanf("%d",&m1[i][j]);
-        }
-    }
-    pritnf("------FOR MATRIX_2------\n");
-    printf("Enter no. of rows: \n");
-    scanf("%d",&r2);
-    printf("Enter no. of columns: \n");
-    scanf("%d",&c2);
-    printf("Enter elements of Matrix 2: \n");
-    for(int i=0;i<r2;i++)
-    {
-        for(int j=0;j<c2;j++)
-        {
-            scanf("%d",&m2[i][j]);
-        }
-    }
-
-    if(c1==r2)
-    {
-      int res[r1][c2];
-      for(int i=0;i<r1;i++)
+      for(int j=0;j<c2;j++)
       {
-        for(int j=0;j<c2;j++)
+        res[i][j]=0;
+        for(int k=0;k<cr;k++)
         {
-            res[i][j] = 
+          res[i][j]+=arr1[i][k]*arr2[k][j];
         }
       }
     }
-    else
+  printf("Resultant Matrix: \n");
+  for(int i=0;i<r1;i++)
+  {
+    for(int j=0;j<c2;j++)
     {
-      printf("Multiplication not possible.");
+      printf("%d ",res[i][j]);
     }
-    return 0;
+    printf("\n");
+  }
+  }
+  return 0;
 }
---
+++ Arrays/2D/waveprinting.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+int main()
+{
+    int n;
+    printf("Enter the size of array: ");
+    scanf("%d",&n);
+    int arr[n][n];
+    printf("Enter elements of array: \n");
+    for(int i=0;i<n;i++)
+    {
+        for(int j=0;j<n;j++)
+        {
+            scanf("%d",&arr[i][j]);
+        }
+    }
+
+    for(int i=0;i<n;i++)
+    {
+        if(i%2==0)
+        {
+            for(int j=0;j<n;j++)
+            {
+                printf("%d ",arr[i][j]);
+            }
+        }
+        else
+        {
+          for(int j=n-1;j>=0;j--)
+          {
+            printf("%d ",arr[i][j]);
+          }
+        }
+        printf("\n");
+    }
+    return 0;
+}
